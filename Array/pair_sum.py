86c64c699e5fe7bf8258a3cc45a0b013b6129485
Duc Filan <ducfilan@gmail.com>
2017-11-13 20:37:26 +0700

    Add problem descriptions

--- Array/pair_sum.py
+++ Array/pair_sum.py
@@ -1,11 +1,25 @@
+# Given an integer array, output all the unique pairs that sum up to a specific value k.
+#
+# So the input:
+#
+# pair_sum([1,3,2,2],4)
+#
+# would return 2 pairs:
+#
+#  (1,3)
+#  (2,2)
+#
+# NOTE: FOR TESTING PURPOSES CHANGE YOUR FUNCTION SO IT OUTPUTS THE NUMBER OF PAIRS
+
+
 def pair_sum(arr, k):
     count = 0
-    trace = {}
+    trace = set()
 
     for e in arr:
         if e in trace:
             count += 1
         else:
-            trace[k - e] = 0
+            trace.add(k - e)
 
     return count
