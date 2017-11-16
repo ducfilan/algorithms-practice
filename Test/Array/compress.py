63600bca9d8a47cebe35c3c2fc3695fae0a905f9
Duc Filan <ducfilan@gmail.com>
2017-11-16 11:43:41 +0700

    Add more tests to string compression

--- Test/Array/compress.py
+++ Test/Array/compress.py
@@ -7,6 +7,9 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(sol(''), '')
         self.assertEqual(sol('AABBCC'), 'A2B2C2')
         self.assertEqual(sol('AAABCCDDDDD'), 'A3B1C2D5')
+        self.assertEqual(sol('AAAABBBBCCCCCDDEEEE'), 'A4B4C5D2E4')
+        self.assertEqual(sol('AAB'), 'A2B1')
+        self.assertEqual(sol('A'), 'A1')
 
 
 if __name__ == '__main__':
