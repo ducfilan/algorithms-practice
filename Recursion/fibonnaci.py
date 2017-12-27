2f4b7124d381e44088350948ac70b74ee8ec7ffa
Duc Filan <ducfilan@gmail.com>
2017-12-27 10:59:07 +0700

    Add memoization & bottom up solutions to fibonnaci problem

--- Recursion/fibonnaci.py
+++ Recursion/fibonnaci.py
@@ -7,3 +7,13 @@ def fib_rec(n, memo=None):
         return memo[n - 1]
 
     return fib_rec(n - 1, memo) + fib_rec(n - 2, memo)
+
+
+def fib_bottom_up(n):
+    fib_numbers = [0, 1, 1]
+    fib_numbers += [0] * (n - 2)
+
+    for i in range(3, n + 1):
+        fib_numbers[i] = fib_numbers[i - 1] + fib_numbers[i - 2]
+
+    return fib_numbers[n]
--- Test/Recursion/fibonnaci.py
+++ Test/Recursion/fibonnaci.py
@@ -1,5 +1,5 @@
 import unittest
-from Recursion.fibonnaci import fib_rec as solution
+from Recursion.fibonnaci import fib_rec as solution, fib_bottom_up
 
 
 class MyTestCase(unittest.TestCase):
@@ -7,6 +7,11 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(solution(10), 55)
         self.assertEqual(solution(1), 1)
         self.assertEqual(solution(23), 28657)
+
+    def test_fib_bottom_up(self):
+        self.assertEqual(fib_bottom_up(10), 55)
+        self.assertEqual(fib_bottom_up(1), 1)
+        self.assertEqual(fib_bottom_up(23), 28657)
 
 
 if __name__ == '__main__':
