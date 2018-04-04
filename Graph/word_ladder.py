a5256323c6bc0786d77fc7bbf37427d024150238
Duc Filan <ducfilan@gmail.com>
2018-04-04 18:58:09 +0900

    word_ladder problem - Fix find shortest path

--- Graph/word_ladder.py
+++ Graph/word_ladder.py
@@ -42,3 +42,39 @@ def bfs(start):
             vertices_queue.enqueue(neighbor)
 
         current_vertex.set_color('black')
+
+
+def find_shortest(start, end):
+    shortest_distance = None
+
+    start.set_distance(0)
+    start.set_predecessor_vertex(None)
+
+    vertices_queue = Queue()
+    vertices_queue.enqueue(start)
+
+    while vertices_queue.size() > 0:
+        current_vertex = vertices_queue.dequeue()
+
+        for neighbor in current_vertex.get_connections():
+            if neighbor.get_color() == 'black':
+                continue
+
+            neighbor_distance = current_vertex.get_distance() + 1
+
+            if neighbor == end and (not shortest_distance or neighbor_distance < shortest_distance):
+                shortest_distance = neighbor_distance
+
+            neighbor.set_color('grey')
+            neighbor.set_distance(neighbor_distance)
+            neighbor.set_predecessor_vertex(current_vertex)
+
+            vertices_queue.enqueue(neighbor)
+
+        current_vertex.set_color('black')
+
+    return shortest_distance
+
+
+g = build_graph(["FOOL", "POOL", "POLL", "POLE", "PALE", "SALE", "SAGE"])
+print(find_shortest(g.get_vertex("FOOL"), g.get_vertex("SAGE")))
