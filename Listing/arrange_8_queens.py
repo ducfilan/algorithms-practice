8cbe0a9ae2a1ade284d4f55aa014b879f82e417b
Duc Filan <ducfilan@gmail.com>
2020-02-19 23:39:15 +0900

    Fix 8 queens problem

--- Listing/arrange_8_queens.py
+++ Listing/arrange_8_queens.py
@@ -5,22 +5,29 @@ def arrange_8_queens(queen_no=1):
     for i in range(1, 9):
         is_next = False
         for r in result:
-            if i in r or r[0] + r[1] == i + queen_no or r[0] - r[1] == i - queen_no:
+            if i == r[0] or r[0] + r[1] == i + queen_no or r[0] - r[1] == i - queen_no:
                 is_next = True
-                continue
+                break
+
         if is_next:
             if i == 8:
-                del result[:]
+                element_to_del = result[-1]
+                if element_to_del[0] == 8:
+                    del result[-2:]
+                else:
+                    del result[-1]
 
             continue
 
-        if len(result) <= queen_no:
-            result.append((i, queen_no))
-        else:
-            result[queen_no] = (i, queen_no)
+        result.append((i, queen_no))
 
         if len(result) == 8:
             print(result)
+            element_to_del = result[-1]
+            if element_to_del[0] == 8:
+                del result[-2:]
+            else:
+                del result[-1]
         elif queen_no < 8:
             arrange_8_queens(queen_no + 1)
 
