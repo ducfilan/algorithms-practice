8ac9549195c3eb8312e75c9d211aba2d93d8f0a0
Duc Filan <ducfilan@gmail.com>
2020-04-01 16:33:50 +0900

    Add description for lengthOfLongestSubstringKDistinct problem

--- Array/longest_substring_k_distinct.py
+++ Array/longest_substring_k_distinct.py
@@ -1,3 +1,8 @@
+# Level: Hard
+# Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+# For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s, k):
         if s == "" or k == 0:
