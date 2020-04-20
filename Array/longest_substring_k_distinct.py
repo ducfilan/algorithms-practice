d7fba676095b4332b5956493381f253552310887
Duc Filan <ducfilan@gmail.com>
2020-04-20 21:28:45 +0900

    Rewrite get_max_length_substring_size_k problem

--- Array/longest_substring_k_distinct.py
+++ Array/longest_substring_k_distinct.py
@@ -39,9 +39,55 @@ class Solution:
 
         return s[max_start:max_end + 1]
 
+    def _init_key_or_increase_value(self, d, key):
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+
+    def get_max_length_substring_size_k(self, s, k):
+        if not s or len(s) == 0 or k <= 0:
+            return 0
+
+        # Identify the window start and end.
+        substring_chars = {}
+        start, end = 0, 0
+
+        while True:
+            self._init_key_or_increase_value(substring_chars, s[end])
+            end += 1
+
+            if len(substring_chars) == k:
+                break
+
+        # Init the max and current of substring.
+        max_len = current_len = len(substring_chars)
+
+        # Slide the [start, end] window to find the max length of the substring.
+        while end < len(s):
+            self._init_key_or_increase_value(substring_chars, s[end])
+
+            while len(substring_chars) > k:
+                substring_chars[s[start]] -= 1
+                if substring_chars[s[start]] == 0:
+                    del substring_chars[s[start]]
+                start += 1
+
+            current_len = end - start + 1
+
+            max_len = max(max_len, current_len)
+            end += 1
+
+        return max_len
+
 
 s = Solution()
 
+print('pass' if s.get_max_length_substring_size_k('', 2) == 0 else 'fail')
+print('pass' if s.get_max_length_substring_size_k('abc', 0) == 0 else 'fail')
+print('pass' if s.get_max_length_substring_size_k('abc', 1) == 1 else 'fail')
+print('pass' if s.get_max_length_substring_size_k('abcbcaddadaadefg', 2) == 8 else 'fail')
+print('---')
 print('pass' if s.lengthOfLongestSubstringKDistinct('', 2) == '' else 'fail')
 print('pass' if s.lengthOfLongestSubstringKDistinct('abc', 0) == '' else 'fail')
 print('pass' if s.lengthOfLongestSubstringKDistinct('abc', 1) == 'a' else 'fail')
