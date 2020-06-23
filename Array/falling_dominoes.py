e8569b361a31bb2690de1608cda2f2936ca31e98
Duc Filan <ducfilan@gmail.com>
2020-06-23 22:57:36 +0900

    Add problem falling dominoes

---
+++ Array/falling_dominoes.py
@@ -0,0 +1,71 @@
+# Hi, here's your problem today. This problem was recently asked by Twitter:
+#
+# Given a string with the initial condition of dominoes, where:
+#
+# . represents that the domino is standing still
+# L represents that the domino is falling to the left side
+# R represents that the domino is falling to the right side
+#
+# Figure out the final position of the dominoes. If there are dominoes that get pushed on both ends, the force cancels out and that domino remains upright.
+#
+# Example:
+# Input:  ..R...L..R.
+# Output: ..RR.LL..RR
+# Here is your starting point:
+#
+# class Solution(object):
+#   def pushDominoes(self, dominoes):
+#     # Fill this in.
+#
+# print Solution().pushDominoes('..R...L..R.')
+# # ..RR.LL..RR
+
+
+class Solution(object):
+    def _change_state(self, current_state, falling_positions):
+        next_falling_positions = []
+
+        for position in falling_positions:
+            current_position_state = current_state[position]
+
+            is_current_position_balanced = len(current_state) >= 3 and \
+                ((
+                    position >= 2 and
+                    current_position_state == 'L' and
+                    current_state[position - 1] == '.' and
+                    current_state[position - 2] == 'R'
+                ) or
+                    (
+                    position <= len(current_state) - 3 and
+                    current_position_state == 'R' and
+                    current_state[position + 1] == '.' and
+                    current_state[position + 2] == 'L'
+                ))
+
+            if is_current_position_balanced:
+                continue
+            elif current_position_state == 'L' and position > 0:
+                current_state[position - 1] = 'L'
+                next_falling_positions.append(position - 1)
+            elif current_position_state == 'R' and position < (len(current_state) - 1):
+                current_state[position + 1] = 'R'
+                next_falling_positions.append(position + 1)
+
+        if not next_falling_positions:
+            return current_state
+
+        return self._change_state(current_state, next_falling_positions)
+
+    def pushDominoes(self, dominoes):
+        falling_positions = [i for i, domino in enumerate(dominoes) if domino == 'L' or domino == 'R']
+
+        return ''.join(self._change_state(list(dominoes), falling_positions))
+
+
+print(Solution().pushDominoes('..R...L..R.'))
+# ..RR.LL..RR
+
+print(Solution().pushDominoes(''))
+print(Solution().pushDominoes('L'))
+print(Solution().pushDominoes('LL'))
+print(Solution().pushDominoes('R.L'))
