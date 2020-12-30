8a97adcaaf2d2607374539330a73f793d13c8eb3
Duc Filan <ducfilan@gmail.com>
2020-12-30 16:25:15 +0900

    Fix find k largest numbers

--- Tree/find_k_largest_numbers.py
+++ Tree/find_k_largest_numbers.py
@@ -6,14 +6,17 @@ from heapq import heappop, heappush
 def find_k_largest_numbers(nums, k):
     top_k_largest = []
 
-    for num in nums[:k]:
+    for num in nums[:k + 1]:
         heappush(top_k_largest, num)
 
-    for num in nums[k:]:
+    for num in nums[k + 1:]:
         heappop(top_k_largest)
         heappush(top_k_largest, num)
+
+    heappop(top_k_largest)
 
     return top_k_largest
 
 
 print(find_k_largest_numbers([3, 1, 5, 12, 2, 11], 3))
+print(find_k_largest_numbers([11, 12, 5, 3, 1, 2], 3))
