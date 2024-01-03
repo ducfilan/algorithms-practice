0d0fab8aaaadafb0593cf589c86089d8fd465318
Duc Filan <ducfilan@gmail.com>
2024-01-04 00:00:51 +0800

    Create numberOfProvinces.go

---
+++ Graph/numberOfProvinces.go
@@ -0,0 +1,52 @@
+/*
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+
+ 
+
+Example 1:
+
+
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2
+Example 2:
+
+
+Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+*/
+
+func findCircleNum(isConnected [][]int) int {
+    n := len(isConnected)
+    visited := make([]bool, n)
+    provinceCount := 0
+
+    var visitAllConnectedCities func (i int)
+    visitAllConnectedCities = func (i int) {
+        if visited[i] {
+            return
+        }
+
+        visited[i] = true
+
+        for j := 0; j < n; j++ {
+            if isConnected[i][j] == 1 && !visited[j] {
+                visitAllConnectedCities(j)
+            }
+        }
+    }
+
+    for i := 0; i < n; i++ {
+        if !visited[i] {
+            visitAllConnectedCities(i)
+            provinceCount++
+        }
+    }
+
+    return provinceCount
+}
